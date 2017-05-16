879049df42930f55fc04ea9a0a26ef312bd37e52
zhuwenzhuang <zhuwenzhuang@192.168.5.101>
2017-05-16 14:22:40 +0800

    finish the course

--- BcAssignment3starterCode/assignment3starterCode/BlockHandler.java
+++ BcAssignment3starterCode/assignment3starterCode/BlockHandler.java
@@ -1,47 +1,100 @@
 
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Map;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import sun.security.rsa.RSAPublicKeyImpl;
 
 public class BlockHandler {
-    private BlockChain blockChain;
+	private BlockChain blockChain;
 
-    /** assume blockChain has the genesis block */
-    public BlockHandler(BlockChain blockChain) {
-        this.blockChain = blockChain;
-    }
+	/** assume blockChain has the genesis block */
+	public BlockHandler(BlockChain blockChain) {
+		this.blockChain = blockChain;
+	}
 
-    /**
-     * add {@code block} to the block chain if it is valid.
-     * 
-     * @return true if the block is valid and has been added, false otherwise
-     */
-    public boolean processBlock(Block block) {
-        if (block == null)
-            return false;
-        return blockChain.addBlock(block);
-    }
+	public static final String KEY_ALGORITHM = "RSA";
 
-    /** create a new {@code block} over the max height {@code block} */
-    public Block createBlock(PublicKey myAddress) {
-        Block parent = blockChain.getMaxHeightBlock();
-        byte[] parentHash = parent.getHash();
-        Block current = new Block(parentHash, myAddress);
-        UTXOPool uPool = blockChain.getMaxHeightUTXOPool();
-        TransactionPool txPool = blockChain.getTransactionPool();
-        TxHandler handler = new TxHandler(uPool);
-        Transaction[] txs = txPool.getTransactions().toArray(new Transaction[0]);
-        Transaction[] rTxs = handler.handleTxs(txs);
-        for (int i = 0; i < rTxs.length; i++)
-            current.addTransaction(rTxs[i]);
+	public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException {
+		KeyPairGenerator kpg = KeyPairGenerator.getInstance(KEY_ALGORITHM);
+		kpg.initialize(1024);// 1024?
+		KeyPair kp = kpg.generateKeyPair();
 
-        current.finalize();
-        if (blockChain.addBlock(current))
-            return current;
-        else
-            return null;
-    }
+		RSAPublicKey pk = (RSAPublicKey) kp.getPublic();
+		RSAPrivateKey sk = (RSAPrivateKey) kp.getPrivate();
+		System.out.println(pk + "\n" + sk);
+		{
+			Cipher cipher = null;
+			try {
+				cipher = Cipher.getInstance(KEY_ALGORITHM);
+				cipher.init(cipher.ENCRYPT_MODE, pk);// 公钥、私钥，加密、解密都可以
+				byte[] out = cipher.doFinal(new byte[10]);
+			} catch (NoSuchPaddingException e) {
+				e.printStackTrace();
+			} catch (IllegalBlockSizeException e) {
+				e.printStackTrace();
+			} catch (BadPaddingException e) {
+				e.printStackTrace();
+			}
+		}
 
-    /** process a {@code Transaction} */
-    public void processTx(Transaction tx) {
-        blockChain.addTransaction(tx);
-    }
+		Block b = new Block(null, pk);
+		BlockChain bc = new BlockChain(b);
+		BlockHandler bh = new BlockHandler(bc);
+		Transaction t = new Transaction();
+		t.addInput(b.getCoinbase().getHash(), 0);
+		t.addOutput(25, pk);
+		t.addSignature(Crypto.signature(sk, t.getRawDataToSign(0)), 0);
+		t.finalize();
+		bh.processTx(t);
+		b = bh.createBlock(pk);
+		bh.processBlock(b);
+	}
+
+	/**
+	 * add {@code block} to the block chain if it is valid.
+	 * 
+	 * @return true if the block is valid and has been added, false otherwise
+	 */
+	public boolean processBlock(Block block) {
+		if (block == null)
+			return false;
+		return blockChain.addBlock(block);
+	}
+
+	/** create a new {@code block} over the max height {@code block} */
+	public Block createBlock(PublicKey myAddress) {
+		Block parent = blockChain.getMaxHeightBlock();
+		byte[] parentHash = parent.getHash();
+		Block current = new Block(parentHash, myAddress);
+		UTXOPool uPool = blockChain.getMaxHeightUTXOPool();
+		TransactionPool txPool = blockChain.getTransactionPool();
+		TxHandler handler = new TxHandler(uPool);
+		Transaction[] txs = txPool.getTransactions().toArray(new Transaction[0]);
+		Transaction[] rTxs = handler.handleTxs(txs);
+		for (int i = 0; i < rTxs.length; i++)
+			current.addTransaction(rTxs[i]);
+
+		current.finalize();
+		if (blockChain.addBlock(current))
+			return current;
+		else
+			return null;
+	}
+
+	/** process a {@code Transaction} */
+	public void processTx(Transaction tx) {
+		blockChain.addTransaction(tx);
+	}
 }
---
+++ BcAssignment3starterCode/assignment3starterCode/TxHandler.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+
+
+public class TxHandler {
+
+    /**
+     * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
+     * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
+     * constructor.
+     */
+	private UTXOPool pool;
+    public TxHandler(UTXOPool utxoPool) {
+    	pool = new UTXOPool(utxoPool);
+    }
+    
+    public UTXOPool getUTXOPool(){
+    	return pool;
+    }
+    /**
+     * @return true if:
+     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
+     * (2) the signatures on each input of {@code tx} are valid, 
+     * (3) no UTXO is claimed multiple times by {@code tx},
+     * (4) all of {@code tx}s output values are non-negative, and
+     * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
+     *     values; and false otherwise.
+     */
+    public boolean isValidTx(Transaction tx) {
+    	ArrayList<Transaction.Input> inputs = tx.getInputs();
+    	HashSet<UTXO> findDuplicate = new HashSet<>();
+    	int index = -1;
+    	double inputValue = 0;
+    	double outputValue = 0;
+		for (Transaction.Input in : inputs) {
+			index++;
+			UTXO unusedTxnOutput = new UTXO(in.prevTxHash, in.outputIndex);
+			if (!pool.contains(unusedTxnOutput)) {
+				return false;
+			}
+			if (findDuplicate.contains(unusedTxnOutput)) {
+				return false;
+			}
+			findDuplicate.add(unusedTxnOutput);
+			Transaction.Output lastOutput = pool.getTxOutput(unusedTxnOutput);
+			if (!Crypto.verifySignature(lastOutput.address, tx.getRawDataToSign(index), in.signature)) {
+				return false;
+			}
+			inputValue += lastOutput.value;
+		}
+		for (Transaction.Output output : tx.getOutputs()) {
+			if (output.value < 0) {
+				return false;
+			}
+			outputValue += output.value;
+		}
+    	if(outputValue>inputValue){
+    		return false;
+    	}
+    	return true;
+    }
+
+    /**
+     * Handles each epoch by receiving an unordered array of proposed transactions, checking each
+     * transaction for correctness, returning a mutually valid array of accepted transactions, and
+     * updating the current UTXO pool as appropriate.
+     */
+    public Transaction[] handleTxs(Transaction[] possibleTxs) {
+		ArrayList<Transaction> validTxn = new ArrayList<>();
+		for (Transaction tx : possibleTxs) {
+			if (isValidTx(tx)) {
+				validTxn.add(tx);
+				for (Transaction.Input in : tx.getInputs()) {
+					UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
+					pool.removeUTXO(utxo);
+				}
+				for (int i = 0; i < tx.getOutputs().size(); i++) {
+					Transaction.Output output = tx.getOutputs().get(i);
+					byte[] hash = tx.getHash();
+					pool.addUTXO(new UTXO(hash, i), output);
+				}
+			}
+		}
+		return validTxn.toArray(new Transaction[validTxn.size()]);
+	}
+
+}
